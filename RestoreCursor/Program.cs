ad883f5e23bc7298b36ef6edd74c3f0f3c471daa
William Krebbers <william.krebbers@gmail.com>
2016-02-20 19:29:03 +0100

    first commit

---
+++ RestoreCursor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoreCursor
+{
+    class Program
+    {
+        const int SPI_SETCURSORS = 0x0057;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDCHANGE = 0x02;
+
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+        public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, uint pvParam, uint fWinIni);
+        static void Main(string[] args)
+        {
+            SystemParametersInfo(SPI_SETCURSORS, 0, 0, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+        }
+    }
+}
